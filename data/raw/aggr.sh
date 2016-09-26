7a2dfa141a9f90d32b671af5b905dd8c2095abda
leosum1 <ngenibre@gmail.com>
2016-09-26 13:59:30 -0400

    Finish Cleaning Aggr and Volumes
    Finish Cleaning Aggr and Volumes

--- data/raw/aggr.sh
+++ data/raw/aggr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# author nicolas.genibre@citrix.com
-# date 9/20/2016
-# version 1.0
+# author: nicolas.genibre@citrix.com
+# date: 9/20/2016
+# version: 1.0
 
+# Place yourself in the directory containing the raw data
 # tidy dataset output
 > aggr.data
 
@@ -10,7 +11,7 @@
 echo "Time,Filer,Aggregate,Total,Used,Free,UsageRatio" >> aggr.data
 
 # Clean data for netapp-02
-# make sure to know the number of aggregate and change the j loop with the right number
+# make sure to know the number of aggregate and change the j loop with the right numbering
 for i in `ls | grep 54`; do
 	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/BEGIN Iteration/,/[[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]/p' | sed -n -e 's/.*\([[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]\).*/\1/p' > date1.out
 	while read line; do for j in {1 2}; do echo "$line,netapp-02"; done; done < date1.out > date.out
@@ -21,7 +22,7 @@ for i in `ls | grep 54`; do
 done
 
 # Clean data for netapp-01
-# make sure to know the number of aggregate and change the j loop with the right number
+# make sure to know the number of aggregate and change the j loop with the right numbering
 for i in `ls | grep 53`; do
 	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/BEGIN Iteration/,/[[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]/p' | sed -n -e 's/.*\([[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]\).*/\1/p' > date1.out
 	while read line; do for j in {1 2 3}; do echo "$line,netapp-01"; done; done < date1.out > date.out
@@ -31,6 +32,7 @@ for i in `ls | grep 53`; do
 	rm -rf date.out aggr.out
 done
 
-# Move out clean data of raw directory 
+# Move out clean data from raw directory 
 cat aggr.data | sed  -n -e 's/,.$/ /p' > ../aggr.data
+dos2unix ../aggr.data
 rm -rf aggr.data
---
+++ data/raw/vol.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# author nicolas.genibre@citrix.com
+# date 9/20/2016
+# version 1.0
+
+# Place yourself in the directory containing the raw data
+# tidy dataset output
+> vol.data
+
+# Add headers
+echo "Time,Filer,Volume,Total,Used,Free,UsageRatio" >> vol.data
+
+# Clean data for netapp-02
+# make sure to know the number of volume (not coun ting vol0) and change the j loop with the right number
+for i in `ls | grep 54`; do
+	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/BEGIN Iteration/,/[[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]/p' | sed -n -e 's/.*\([[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]\).*/\1/p' > date1.out
+	while read line; do for j in {1 2 3 4 5 6 7 8 9 10 11}; do echo "$line,netapp-02"; done; done < date1.out > date.out
+	rm -rf date1.out
+	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/POSTSTATS =-=-=-=-=-= df -x -m/,/--- EXE-TIME of /p' | sed -n -e '/vs1_/p' | sed -n -e 's/[A-Z][A-Z]//gp' | sed -n -e 's/%//gp' | sed -n -e 's/\/vol\///gp' | sed -n -e 's/\///gp' | awk '{print $1","$2","$3","$4}' > vol.out
+	paste -d"," date.out vol.out >> vol.data
+	rm -rf date.out vol.out
+done
+
+# Clean data for netapp-01
+# make sure to know the number of aggregate and change the j loop with the right number
+for i in `ls | grep 53`; do
+	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/BEGIN Iteration/,/[[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]/p' | sed -n -e 's/.*\([[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]\).*/\1/p' > date1.out
+	while read line; do for j in {1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24}; do echo "$line,netapp-01"; done; done < date1.out > date.out
+	rm -rf date1.out
+	sed -n -e '/BEGIN Iteration/,/END Iteration/p' $i | sed -n -e '/POSTSTATS =-=-=-=-=-= df -x -m/,/--- EXE-TIME of /p' | sed -n -e '/vs1_/p' | sed -n -e 's/[A-Z][A-Z]//gp' | sed -n -e 's/%//gp' | sed -n -e 's/\/vol\///gp' | sed -n -e 's/\///gp' | awk '{print $1","$2","$3","$4}' > vol.out
+	paste -d"," date.out vol.out >> vol.data
+	rm -rf date.out vol.out
+done
+
+# Move out clean data from raw directory 
+cat vol.data > ../vol.data
+dos2unix ../vol.data
+rm -rf vol.data
